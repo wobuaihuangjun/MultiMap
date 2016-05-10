f530d691c116f26b4baa100927e7a148a7daaec7
wobuaihuangjun <892105817@qq.com>
2016-05-10 16:43:27 +0800

    Initial commit

---
+++ app/src/main/java/com/huangzj/multimap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.huangzj.multimap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
